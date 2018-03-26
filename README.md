a42423b6b0cdfc6bdfd32f124384322f97cf7bbc
Sondre Gjellestad <sondre.gjellestad@hotmail.com>
2018-03-26 22:50:56 +0200

    :tada: Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# User Agent API
+
+Simple useragent API with Express, for an assignment on freeCodeCamp
---
+++ server.js
@@ -0,0 +1,23 @@
+const
+	http = require('http'),
+	https = require('https')
+	express = require('express'),
+	useragent = require('express-useragent')
+	
+const port = process.env.PORT || 8080
+
+const app = express()
+
+app.use(useragent.express())
+
+app.get('/api/whoami', (req, res) => {
+	res.json({
+		ipaddress: req.ip,
+		language: req.acceptsLanguages()[0],
+		software: req.useragent.platform + ', ' + req.useragent.os
+	})
+})
+
+http // Start HTTP server
+	.createServer(app)
+	.listen(port, () => console.log('HTTP listening on port', port))
